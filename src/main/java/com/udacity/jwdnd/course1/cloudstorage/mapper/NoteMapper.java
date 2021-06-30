f76f61e6ef27c37fa9e8bb37aa95f845494965ae
tiger <yunhao@live.unc.edu>
2021-06-30 14:23:39 -0400

    ewfwewfewfe

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
@@ -1,16 +1,16 @@
 package com.udacity.jwdnd.course1.cloudstorage.mapper;
 import com.udacity.jwdnd.course1.cloudstorage.model.Notes;
-import com.udacity.jwdnd.course1.cloudstorage.model.Users;
+
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface NoteMapper {
-    @Select("SELECT * FROM NOTES WHERE username = #{username}")
-    Users getUser(String username);
+    @Select("SELECT * FROM NOTES")
+    Notes getNote(String username);
 
-    @Insert("INSERT INTO USERS (username, salt, password, firstname, lastname) VALUES(#{username}, #{salt}, #{password}, #{firstname}, #{lastname})")
-    @Options(useGeneratedKeys = true, keyProperty = "userid")
-    int insert(Users user);
+    @Insert("INSERT INTO NOTES (notetitle, notedescription, userid) VALUES(#{notetitle}, #{notedescription}, #{userid})")
+    @Options(useGeneratedKeys = true, keyProperty = "noteid")
+    int insert(Notes note);
 }

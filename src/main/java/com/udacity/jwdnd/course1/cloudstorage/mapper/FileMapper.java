db352ea7f92ead5bad3fe10c0b35b1d655e26428
tiger <yunhao@live.unc.edu>
2021-07-01 10:29:35 -0400

    ewfwewfewfe

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/FileMapper.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/FileMapper.java
@@ -5,6 +5,8 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
+
+@Mapper
 public interface FileMapper {
     @Select("SELECT * FROM FILES")
     Files getFile();

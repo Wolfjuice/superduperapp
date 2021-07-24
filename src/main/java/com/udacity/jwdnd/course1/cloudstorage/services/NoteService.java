eb1908e6fa872259c0dcd4d310d008caed791a4b
tiger <yunhao@live.unc.edu>
2021-07-24 12:11:56 -0400

    ZZZZZZZ

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/NoteService.java
@@ -0,0 +1,35 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+import com.udacity.jwdnd.course1.cloudstorage.mapper.NoteMapper;
+import com.udacity.jwdnd.course1.cloudstorage.model.NoteForm;
+import com.udacity.jwdnd.course1.cloudstorage.model.Notes;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Service
+public class NoteService {
+    private NoteMapper noteMapper;
+    public NoteService(NoteMapper noteMapper) {
+        this.noteMapper = noteMapper;
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        System.out.println("Creating NoteService bean");
+    }
+
+    public void addNote(NoteForm noteForm) {
+        Notes note = new Notes();
+        note.setId(noteForm.getId());
+        note.setDescription(noteForm.getDescription());
+        note.setTitle(noteForm.getTitle());
+        noteMapper.insert(note);
+        System.out.println("Note is Submitted Note is Submitted Note is Submitted Note is Submitted Note is Submitted Note is Submitted");
+    }
+
+    public List<Notes> getNotes() {
+        return noteMapper.getNotes();
+    }
+
+}
